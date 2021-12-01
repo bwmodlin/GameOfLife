29003347035bc93cdfbafdedcee1e1b4ef497770
bwmodlin <b_modlin@coloradocollege.edu>
2021-12-01 10:35:06 -0700

    fixed build routing

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 
 const cors = require('cors')
+const path = require("path");
 app.use(cors())
 
 app.use(express.json())
 app.use(express.static('build'))
+
+
+app.get('*', (request, response) =>
+    response.sendFile(path.resolve('build', 'index.html')))
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
