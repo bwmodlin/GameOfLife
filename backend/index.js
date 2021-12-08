516e6ba87ac45af523f5a5e3e94f3e7853969e07
bwmodlin <b_modlin@coloradocollege.edu>
2021-12-07 19:44:46 -0700

    getting ready for test deployment

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 
 const cors = require('cors')
-const path = require("path");
+const path = require("path")
+
 app.use(cors())
 
 app.use(express.json())
 app.use(express.static('build'))
-
 
 app.get('*', (request, response) =>
     response.sendFile(path.resolve('build', 'index.html')))
