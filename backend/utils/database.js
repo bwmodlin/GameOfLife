f6c093052522557210032728665192c3254cc35c
bwmodlin <b_modlin@coloradocollege.edu>
2021-12-10 00:27:40 -0700

    changing connection string

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 
 const { Pool } = require('pg')
 
-const pool = new Pool()
+const connectionString = process.env.DATABASE_URL
+
+const pool = new Pool({
+    connectionString: connectionString,
+})
 
 const getPresets = (callback) => {
     const text = "SELECT name, board FROM public.presets"
